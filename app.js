d5cb193f1df0f79efa660e4a68bce9cdc6b5a69d
ZangerlA <azangerl@gmail.com>
2020-12-23 19:22:43 +0100

    cleanup mess

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var env = require('dotenv').config();
-
+var env = require('dotenv').config({path: '/db.env'});
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
