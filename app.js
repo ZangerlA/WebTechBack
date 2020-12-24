9d4c1a4c60c31fb5e78aa15ba91ce1ab7f5fd361
ZangerlA <azangerl@gmail.com>
2020-12-24 17:14:30 +0100

    rework sequelize config file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var env = require('dotenv')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var { models, sequelize } = require('./models/index.js')
@@ -14,7 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-env.config();
+
 
 sequelize.sync().then(() => {
   console.log(process.env.DATABASE)
---
+++ config/config.js
@@ -0,0 +1,28 @@
+var env = require('dotenv').config()
+
+const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+
+module.exports = {
+    "development": {
+        "username": DB_USERNAME,
+        "password": DB_PASSWORD,
+        "database": "TeamkillLocalDB_dev",
+        "host": DB_HOST,
+        "dialect": "postgres"
+
+    },
+    "test": {
+        "username": DB_USERNAME,
+        "password": DB_PASSWORD,
+        "database": "TeamkillLocalDB_test",
+        "host": DB_HOST,
+        "dialect": "postgres"
+    },
+    "production": {
+        "username": DB_USERNAME,
+        "password": DB_PASSWORD,
+        "database": "TeamkillLocalDB_prod",
+        "host": DB_HOST,
+        "dialect": "postgres"
+    }
+}
