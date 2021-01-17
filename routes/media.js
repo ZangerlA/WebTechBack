3f1880b3c540c0ba1fd8cf83179bbd60469743b4
ZangerlA <azangerl@gmail.com>
2021-01-17 14:21:55 +0100

    bugfix media get route

--- routes/media.js
+++ routes/media.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require('../models');
 
-router.get('/:type?', async function (req, res, next) {
+router.get('/', async function (req, res, next) {
 	let media;
 	try {
 		if (req.query.type) {
